b57e87af8c35595354e5331610e4fc54c09a8f2d
asterized <asterized@space>
2026-07-12 00:56:19 -0500

    chacha20: port NEON optimizations

---
+++ src/libsodium/crypto_stream/chacha20/dolbeau-neon/chacha20_dolbeau-neon.c
@@ -0,0 +1,157 @@
+#include "private/common.h"
+#include "chacha20_dolbeau-neon.h"
+#include "crypto_stream_chacha20.h"
+#include "utils.h"
+
+#if (defined(__aarch64__) || defined(_M_ARM64)) && \
+    (defined(__ARM_NEON) || defined(__ARM_NEON__))
+
+#include <arm_neon.h>
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct chacha_ctx {
+    uint32_t input[16];
+} chacha_ctx;
+
+static void
+chacha_keysetup(chacha_ctx *ctx, const uint8_t *k)
+{
+    ctx->input[0]  = 0x61707865;
+    ctx->input[1]  = 0x3320646e;
+    ctx->input[2]  = 0x79622d32;
+    ctx->input[3]  = 0x6b206574;
+    ctx->input[4]  = LOAD32_LE(k + 0);
+    ctx->input[5]  = LOAD32_LE(k + 4);
+    ctx->input[6]  = LOAD32_LE(k + 8);
+    ctx->input[7]  = LOAD32_LE(k + 12);
+    ctx->input[8]  = LOAD32_LE(k + 16);
+    ctx->input[9]  = LOAD32_LE(k + 20);
+    ctx->input[10] = LOAD32_LE(k + 24);
+    ctx->input[11] = LOAD32_LE(k + 28);
+}
+
+static void
+chacha_ivsetup(chacha_ctx *ctx, const uint8_t *iv, const uint8_t *counter)
+{
+    ctx->input[12] = counter == NULL ? 0 : LOAD32_LE(counter + 0);
+    ctx->input[13] = counter == NULL ? 0 : LOAD32_LE(counter + 4);
+    ctx->input[14] = LOAD32_LE(iv + 0);
+    ctx->input[15] = LOAD32_LE(iv + 4);
+}
+
+static void
+chacha_ietf_ivsetup(chacha_ctx *ctx, const uint8_t *iv, const uint8_t *counter)
+{
+    ctx->input[12] = counter == NULL ? 0 : LOAD32_LE(counter);
+    ctx->input[13] = LOAD32_LE(iv + 0);
+    ctx->input[14] = LOAD32_LE(iv + 4);
+    ctx->input[15] = LOAD32_LE(iv + 8);
+}
+
+void chacha20_encrypt_bytes(chacha_ctx *x_,const uint8_t *m,uint8_t *c_,uint32_t bytes)
+{
+  uint8_t output[64];
+  int i;
+  uint32_t* x = (uint32_t*)&x_->input;
+  uint8_t* out = c_;
+  if (!bytes) return;
+
+#include "u4.h"
+#include "u1.h"
+#include "u0.h"
+}
+
+static int
+stream_ref(unsigned char *c, unsigned long long clen, const unsigned char *n,
+           const unsigned char *k)
+{
+    struct chacha_ctx ctx;
+
+    if (!clen) {
+        return 0;
+    }
+    COMPILER_ASSERT(crypto_stream_chacha20_KEYBYTES == 256 / 8);
+    chacha_keysetup(&ctx, k);
+    chacha_ivsetup(&ctx, n, NULL);
+    memset(c, 0, clen);
+    chacha20_encrypt_bytes(&ctx, c, c, clen);
+    sodium_memzero(&ctx, sizeof ctx);
+
+    return 0;
+}
+
+static int
+stream_ietf_ext_ref(unsigned char *c, unsigned long long clen,
+                    const unsigned char *n, const unsigned char *k)
+{
+    struct chacha_ctx ctx;
+
+    if (!clen) {
+        return 0;
+    }
+    COMPILER_ASSERT(crypto_stream_chacha20_KEYBYTES == 256 / 8);
+    chacha_keysetup(&ctx, k);
+    chacha_ietf_ivsetup(&ctx, n, NULL);
+    memset(c, 0, clen);
+    chacha20_encrypt_bytes(&ctx, c, c, clen);
+    sodium_memzero(&ctx, sizeof ctx);
+
+    return 0;
+}
+
+static int
+stream_ref_xor_ic(unsigned char *c, const unsigned char *m,
+                  unsigned long long mlen, const unsigned char *n, uint64_t ic,
+                  const unsigned char *k)
+{
+    struct chacha_ctx ctx;
+    uint8_t           ic_bytes[8];
+    uint32_t          ic_high;
+    uint32_t          ic_low;
+
+    if (!mlen) {
+        return 0;
+    }
+    ic_high = (uint32_t) (ic >> 32);
+    ic_low  = (uint32_t) ic;
+    STORE32_LE(&ic_bytes[0], ic_low);
+    STORE32_LE(&ic_bytes[4], ic_high);
+    chacha_keysetup(&ctx, k);
+    chacha_ivsetup(&ctx, n, ic_bytes);
+    chacha20_encrypt_bytes(&ctx, m, c, mlen);
+
+    sodium_memzero(&ctx, sizeof ctx);
+
+    return 0;
+}
+
+static int
+stream_ietf_ext_ref_xor_ic(unsigned char *c, const unsigned char *m,
+                           unsigned long long mlen, const unsigned char *n,
+                           uint32_t ic, const unsigned char *k)
+{
+    struct chacha_ctx ctx;
+    uint8_t           ic_bytes[4];
+
+    if (!mlen) {
+        return 0;
+    }
+    STORE32_LE(ic_bytes, ic);
+    chacha_keysetup(&ctx, k);
+    chacha_ietf_ivsetup(&ctx, n, ic_bytes);
+    chacha20_encrypt_bytes(&ctx, m, c, mlen);
+    sodium_memzero(&ctx, sizeof ctx);
+
+    return 0;
+}
+
+struct crypto_stream_chacha20_implementation
+    crypto_stream_chacha20_dolbeau_neon_implementation = {
+        SODIUM_C99(.stream =) stream_ref,
+        SODIUM_C99(.stream_ietf_ext =) stream_ietf_ext_ref,
+        SODIUM_C99(.stream_xor_ic =) stream_ref_xor_ic,
+        SODIUM_C99(.stream_ietf_ext_xor_ic =) stream_ietf_ext_ref_xor_ic
+    };
+
+#endif
---
+++ src/libsodium/crypto_stream/chacha20/dolbeau-neon/u0.h
@@ -0,0 +1,85 @@
+#define vrotlq_u32(a,b) vsliq_n_u32(vshrq_n_u32(a, 32-b),a,b)
+
+  if (!bytes) return;
+  uint8_t partialblock[64];
+    uint32x4_t x_0, x_1, x_2, x_3;
+    uint32x4_t t_1;
+
+    x_0 = vld1q_u32(x +  0);
+    x_1 = vld1q_u32(x +  4);
+    x_2 = vld1q_u32(x +  8);
+    x_3 = vld1q_u32(x + 12);
+
+    for (i = 0 ; i < 20 ; i += 2) {
+      x_0 = vaddq_u32(x_0, x_1);
+      x_3 = veorq_u32(x_3, x_0);
+      x_3 = vrotlq_u32(x_3, 16);
+
+      x_2 = vaddq_u32(x_2, x_3);
+      x_1 = veorq_u32(x_1, x_2);
+
+/*       t_1 = x_1; */
+/*       x_1 = vshlq_n_u32(x_1, 12); */
+/*       t_1 = vshrq_n_u32(t_1, 20); */
+/*       x_1 = veorq_u32(x_1, t_1); */
+      x_1 = vrotlq_u32(x_1, 12);
+
+      x_0 = vaddq_u32(x_0, x_1);
+      x_3 = veorq_u32(x_3, x_0);
+      x_0 = vextq_u32(x_0, x_0, 3);
+      x_3 = vrotlq_u32(x_3, 8);
+
+      x_2 = vaddq_u32(x_2, x_3);
+      x_3 = vextq_u32(x_3, x_3, 2);
+      x_1 = veorq_u32(x_1, x_2);
+      x_2 = vextq_u32(x_2, x_2, 1);
+
+/*       t_1 = x_1; */
+/*       x_1 = vshlq_n_u32(x_1, 7); */
+/*       t_1 = vshrq_n_u32(t_1, 25); */
+/*       x_1 = veorq_u32(x_1, t_1); */
+      x_1 = vrotlq_u32(x_1, 7);
+
+      x_0 = vaddq_u32(x_0, x_1);
+      x_3 = veorq_u32(x_3, x_0);
+      x_3 = vrotlq_u32(x_3, 16);
+
+      x_2 = vaddq_u32(x_2, x_3);
+      x_1 = veorq_u32(x_1, x_2);
+
+/*       t_1 = x_1; */
+/*       x_1 = vshlq_n_u32(x_1, 12); */
+/*       t_1 = vshrq_n_u32(t_1, 20); */
+/*       x_1 = veorq_u32(x_1, t_1); */
+      x_1 = vrotlq_u32(x_1, 12);
+
+      x_0 = vaddq_u32(x_0, x_1);
+      x_3 = veorq_u32(x_3, x_0);
+      x_0 = vextq_u32(x_0, x_0, 1);
+      x_3 = vrotlq_u32(x_3, 8);
+
+      x_2 = vaddq_u32(x_2, x_3);
+      x_3 = vextq_u32(x_3, x_3, 2);
+      x_1 = veorq_u32(x_1, x_2);
+      x_2 = vextq_u32(x_2, x_2, 3);
+
+/*       t_1 = x_1; */
+/*       x_1 = vshlq_n_u32(x_1, 7); */
+/*       t_1 = vshrq_n_u32(t_1, 25); */
+/*       x_1 = veorq_u32(x_1, t_1); */
+      x_1 = vrotlq_u32(x_1, 7);
+    }
+    x_0 = vaddq_u32(x_0, vld1q_u32(x +  0));
+    x_1 = vaddq_u32(x_1, vld1q_u32(x +  4));
+    x_2 = vaddq_u32(x_2, vld1q_u32(x +  8));
+    x_3 = vaddq_u32(x_3, vld1q_u32(x + 12));
+    vst1q_u32((uint32_t*)(partialblock +  0),  x_0);
+    vst1q_u32((uint32_t*)(partialblock + 16),  x_1);
+    vst1q_u32((uint32_t*)(partialblock + 32),  x_2);
+    vst1q_u32((uint32_t*)(partialblock + 48),  x_3);
+    
+    for (i = 0; i < bytes; i++) {
+        out[i] = m[i] ^ partialblock[i];
+    }
+
+    sodium_memzero(partialblock, sizeof partialblock);
